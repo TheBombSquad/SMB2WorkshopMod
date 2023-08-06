ab5ab9d4e1d4f27d9c258166d3452f626666d567
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2023-08-06 17:01:48 -0400

    Hook up packaging script to CLion

--- script/configure-clion-project.sh
+++ script/configure-clion-project.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
+# We assume $PWD is the base directory of the repo
+if [ $(basename "$PWD") = script ] ; then
+cd ..
+fi
+
 # Import external tool configs into Clion
-mkdir -p $PWD/.idea/tools
+mkdir -p "$PWD/.idea/tools"
 cp "$PWD/script/clion-config/customTargets.xml" "$PWD/.idea"
 cp "$PWD/script/clion-config/External Tools.xml" "$PWD/.idea/tools"
 
+# Make our build script executable
+chmod +x "$PWD/script"
 # Generates a compilation database project file for Clion
 echo "Configuring elf2rel..."
 ELF2REL_BUILD=$PWD/dep/ttyd-tools/ttyd-tools/elf2rel/build
-mkdir -p $ELF2REL_BUILD
-cmake -S $ELF2REL_BUILD/.. -B $ELF2REL_BUILD
-bear -- make ELF2REL_BUILD=$ELF2REL_BUILD
+mkdir -p "$ELF2REL_BUILD"
+cmake -S "$ELF2REL_BUILD/.." -B "$ELF2REL_BUILD"
+bear -- make ELF2REL_BUILD="$ELF2REL_BUILD"
---
+++ script/package-project-template.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# To use this script, update the path variables below with valid paths, then run the script
+# If you are using CLion, rename this to `package-project.sh`, then you will be able to build it in CLion
+# Also if you're using CLion: Make sure you run `configure-clion-project.sh` first
+
+# Path to PPCInject - in the future, this should be compiled alongside WSMod
+# Found here: https://github.com/tuckergs/ppc-inject
+PPCINJECT=PATH/TO/YOUR/PPCInject
+
+# Path to your main.dol file from Super Monkey Ball 2
+# You can extract this from an image of your legally obtained copy of Super Monkey Ball 2
+DOL_SRC=PATH/TO/YOUR/main.dol
+
+# Destination path for the patched main.dol file
+# Can be the same path as DOL_SRC, but this will replace the main.dol file
+DOL_DEST=PATH/TO/YOUR/main.dol
+
+# Path to the REL loader code, which is what main.dol is patched with
+# Included in the repo
+RELLOADER_SRC=relloader/iso-rel-loader-us.asm
+
+# Name of the REL file
+# If you change this, you also need to modify the iso-rel-loader-us.asm accordingly
+REL_NAME=mkb2.rel_sample.rel
+
+# Destination of the REL file
+# Ideally you want the path to an extracted root of Super Monkey Ball 2
+# You'll probably want to replace mkb2.rel_sample.rel
+REL_DEST=PATH/TO/YOUR/mkb2.rel_sample.rel
+
+# Pass -i to this script to inject the main.dol file
+if [[ "${1:-}" == "-i" ]]; then
+    echo 'Patching main.dol with ppcinject'
+    "$PPCINJECT" "$DOL_SRC" "$DOL_DEST" "$RELLOADER_SRC"
+fi
+
+make -j$(nproc)
+
+echo 'Copying REL to' $REL_DEST
+cp ApeSphere.us.rel $REL_DEST
