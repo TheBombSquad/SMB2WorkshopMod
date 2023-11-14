6595ade0d0222328d9ce0d66a89fd3272af61a0a
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2023-11-14 12:06:30 -0500

    Fix REL freespace alloc

--- src/internal/relutil.h
+++ src/internal/relutil.h
@@ -6,6 +6,6 @@ namespace relutil {
  * Returns one past the last address of relocation data in mainloop.rel which we are free to
  * overwrite.
  */
-void* compute_mainloop_reldata_boundary();
+void* compute_mainloop_reldata_boundary(void* start);
 
 }// namespace relutil
