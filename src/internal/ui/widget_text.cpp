7f3b68093bc8fe9c84eb1832e7f19daaa15cdaaf
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2023-11-16 00:46:19 -0500

    Make text widgets have automatically determined dimensions

--- src/internal/ui/widget_text.cpp
+++ src/internal/ui/widget_text.cpp
@@ -15,15 +15,20 @@ void Text::disp() {
     m_drop_shadow ? mkb::textdraw_set_drop_shadow() : mkb::textdraw_clear_drop_shadow();
 
     // Vertically fit text to dimensions
+    /*
     const float str_height = mkb::textdraw_get_pixel_height_of_string(const_cast<char*>(m_text));
     const float str_height_scaled = str_height * m_scale.y;
 
     if (str_height_scaled > m_dimensions.y) {
         m_scale.y = m_dimensions.y / str_height_scaled;
     }
+     */
 
     mkb::textdraw_set_scale(m_scale.x, m_scale.y);
-    mkb::textdraw_print_and_fit_to_width(m_dimensions.x, const_cast<char*>(m_text));
+    m_dimensions.x = mkb::textdraw_get_pixel_width_of_string(const_cast<char*>(m_text));
+    m_dimensions.y = mkb::textdraw_get_pixel_height_of_string(const_cast<char*>(m_text));
+    mkb::textdraw_print(const_cast<char*>(m_text));
+    //mkb::textdraw_print_and_fit_to_width(m_dimensions.x, const_cast<char*>(m_text));
 }
 
 }// namespace ui
--- src/internal/ui/widget_text.h
+++ src/internal/ui/widget_text.h
@@ -10,8 +10,8 @@ constexpr mkb::Rgb24 DEFAULT_ORANGE = {0xFF, 0x80, 0x00};
 
 class Text : public Widget {
 public:
-    Text(const char* text, const Vec2d dimensions) : Widget(), m_text(text) { m_dimensions = dimensions; }
-    Text(const char* text, const Vec2d pos, const Vec2d dimensions) : Widget(pos), m_text(text) { m_dimensions = dimensions; }
+    Text(const char* text) : Widget(), m_text(text) {}
+    Text(const char* text, const Vec2d pos) : Widget(pos), m_text(text) {}
     virtual void disp() override;
 
 protected:
