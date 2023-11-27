440877a28539a8ee25a559c707894764e8207005
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2023-11-26 20:55:08 -0500

    Galactic Log now remembers last selected index
    (cherry picked from commit 15a2b1fbedc681c8061556d51695601afaaa8f39)

--- src/internal/ui/widget_menu.h
+++ src/internal/ui/widget_menu.h
@@ -15,6 +15,15 @@ private:
     etl::optional<size_t> m_active_index;
     etl::optional<size_t> m_first_valid_index;
     etl::optional<size_t> m_last_valid_index;
+
+public:
+    const etl::optional<size_t>& get_active_index() const {
+        return m_active_index;
+    }
+
+    void set_active_index(const etl::optional<size_t>& active_index) {
+        m_active_index = active_index;
+    }
 };
 
 }// namespace ui
