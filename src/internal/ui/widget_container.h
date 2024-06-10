dcbbac47d4c51aa77dc971167ef01ae5b3832341
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2024-06-10 02:28:15 -0400

    Add getter/setter for `m_child_depth_step`
    (cherry picked from commit e0ac07fef3ce7bcdeab971192b2373e63b82a91f)

--- src/internal/ui/widget_container.h
+++ src/internal/ui/widget_container.h
@@ -9,7 +9,9 @@ protected:
     static constexpr uint32_t WIDGET_MAX_CHILDREN = 12;
     etl::vector<etl::unique_ptr<Widget>, WIDGET_MAX_CHILDREN> m_children;
 
-    float m_child_depth_step = 0.005;    // When we add a child sprite, by how many units should it be 'in front' of the parent?
+    float m_child_depth_step = 0.005;
+
+    // When we add a child sprite, by how many units should it be 'in front' of the parent?
     Container(const Vec2d pos, const Vec2d dimensions) : Widget(pos, dimensions) {}
 
 public:
@@ -26,6 +28,13 @@ public:
     void remove(const char* label);
     void clear();
     void free_inactive();
+
+    float get_child_depth_step() const {
+        return m_child_depth_step;
+    }
+    void set_child_depth_step(float child_depth_step) {
+        m_child_depth_step = child_depth_step;
+    }
 };
 
 }// namespace ui
