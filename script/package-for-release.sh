2ae68270802e078e35def2611c407373cf271d56
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2024-07-14 00:38:10 -0400

    Update release packaging script

--- script/package-for-release.sh
+++ script/package-for-release.sh
@@ -4,9 +4,14 @@ IFS=$'\n\t'
 
 OUT_NAME=ApeSphere-Custom-v0.4.0.zip
 
+# Script should be run in the root directory of the repo
 mkdir -p out
 make -j$(nproc)
-# TODO: We need to make PPCInject and add the executable here!
+cd dep/ppc-inject
+stack build
+stack install --local-bin-path ./bin
+cd ../..
+cp dep/ppc-inject/bin/* out
 cp LICENSE README.md mkb2.rel_sample.rel docs/music-id-list.txt docs/theme-id-list.txt relloader/iso-rel-loader-us.asm out
 cp configs/default-config.txt out/config.txt
 cp configs/default-authors.txt out/authors.str
